94c1d3a8eba61a8be15bfbf7185306a7d3968094
Your Name <n_fainer@fanshaweonline.ca>
2020-01-29 08:43:34 -0500

    trev

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 
-const port = process.env.PORT || 3030;
+// add socket here
+const io = require('socket.io')();
+
+const port = process.env.PORT || 3000;
 
 // tell express where our static files are (js, images, css etc)
 app.use(express.static('public'));
@@ -10,6 +13,21 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+
+//socket needs a variable for app.listen
 const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
+
+//attach our chat server to our app
+io.attach(server);
+
+io.on('connection'), function(socket) {
+
+    console.log('a user has connected');
+    socket.emit('connected', {sID:  socket.id, message: "new connection"});
+
+    socket.on('disconnect', function() {
+        console.log('a user has disconnected');
+    })
+}
